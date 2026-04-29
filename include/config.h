9c36431c3fca79b062f4df1f68a7a7ffd27be3b0
Jrddlol2 <89295513+Jrddlol2@users.noreply.github.com>
2026-04-30 00:39:53 +0800

    Added new pin assignments

--- include/config.h
+++ include/config.h
@@ -8,11 +8,14 @@
 
 // ================= OUTPUT TEAM PINS ===============
 
-#define NUM_LEDS    16
-#define LED_PIN     6
-#define TFT_CS      10
-#define TFT_DC      9
-#define TFT_RST     8
+#define NUM_LEDS    16     
+#define LED_PIN_1   6       
+#define LED_PIN_2   5      
+
+#define TFT_CS_1    10     
+#define TFT_CS_2    11     
+#define TFT_DC      9       
+#define TFT_RST     8      
 
 // ================= HOST TEAM PINS =================
 
